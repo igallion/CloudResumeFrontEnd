9360a47e22e856929933a678846229043146fb5c
igallion <82122432+igallion@users.noreply.github.com>
2023-05-22 16:22:33 -0400

    Changes to DNS records

--- main.tf
+++ main.tf
@@ -51,20 +51,22 @@ resource "aws_route53_zone" "hostedZoneTerraform" {
 }
 
 resource "aws_route53_record" "NSRecordsTerraform" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert2.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  name            = "ilgallion.com"
-  ttl             = 172800
-  type            = "NS"
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
   zone_id         = aws_route53_zone.hostedZoneTerraform.zone_id
-
-  records = [
-    aws_route53_zone.hostedZoneTerraform.name_servers[0],
-    aws_route53_zone.hostedZoneTerraform.name_servers[1],
-    aws_route53_zone.hostedZoneTerraform.name_servers[2],
-    aws_route53_zone.hostedZoneTerraform.name_servers[3],
-  ]
 }
-
+/*
 resource "aws_route53_record" "A" {
   zone_id = aws_route53_zone.hostedZoneTerraform.zone_id
   name    = "ilgallion.com"
@@ -88,6 +90,7 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = false
   }
 }
+*/
 //S3 bucket
 resource "aws_s3_bucket" "TestBucket" {
   bucket = "ilg-tf-test-bucket"
